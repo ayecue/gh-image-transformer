567d1017ced7df62c96b92a81d7261f562556f1f
ayecue <soerenwehmeier@googlemail.com>
2024-01-04 17:17:25 +0100

    0.1.5 minimize output

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { writeFileSync } from 'fs';
 import Jimp from 'jimp';
 import path from 'path';
 
+export enum OptimizedCodes {
+  White = '#$',
+  Black = '#!',
+  Invisible = '#?'
+}
+
 export function parseMeasurement(arg: string, defaultValue: number = Jimp.AUTO): number {
   if (arg === undefined || arg === '') return defaultValue;
 
@@ -36,25 +42,39 @@ async function generateMatrix(image: Jimp, width: number = 64, height: number = 
     const green = this.bitmap.data[idx + 1];
     const blue = this.bitmap.data[idx + 2];
     const alpha = this.bitmap.data[idx + 3];
-    const hex = withoutAlpha || alpha === 255 ? rgbToHex(red, green, blue) : rgbToHexWithAlpha(red, green, blue, alpha);
+    const hasNoAlpha = withoutAlpha || alpha === 255;
+    let hex;
+
+    if (hasNoAlpha) {
+      hex = rgbToHex(red, green, blue);
+      if (hex === '#000000') hex = OptimizedCodes.Black;
+      else if (hex === '#FFFFFF') hex = OptimizedCodes.White;
+      else if (hex.charAt(1) === hex.charAt(2) && hex.charAt(3) === hex.charAt(4) && hex.charAt(5) === hex.charAt(6)) hex = `#${hex.charAt(1) + hex.charAt(3) + hex.charAt(5)}`;
+    } else {
+      hex = rgbToHexWithAlpha(red, green, blue, alpha);
+      if (alpha === 0) hex = OptimizedCodes.Invisible;
+      else if (hex.charAt(1) === hex.charAt(2) && hex.charAt(3) === hex.charAt(4) && hex.charAt(5) === hex.charAt(6) && hex.charAt(7) === hex.charAt(8)) hex = `#${hex.charAt(1) + hex.charAt(3) + hex.charAt(5) + hex.charAt(7)}`;
+    }
+
     chunks[y][x] = hex;
   });
 
   return chunks;
 }
 
+function generateCharset(size: number = 256, offset: number = 32) {
+  let charset = '';
+  const max = offset + size;
+  for (let i = offset; i < max; i++) charset += String.fromCharCode(i);
+  return charset;
+}
+
 function generateSprites(matrix: string[][], scale: number): string {
   let rowIndex = 0;
-  let output = `<scale=${scale}><size=${scale}><mspace=${scale * 1.1}>`;
+  let output = `"<scale=${scale}><size=${scale}><mspace=${scale * 1.1}>"`;
 
   for (let i = matrix.length - 1; i >= 0; i--) {
-    let line = `<pos=0><voffset=${scale * rowIndex}>`;
-
-    for (const column of matrix[i]) {
-      line += `<sprite=0 color=${column}>`
-    }
-
-    output += line;
+    output += `+¶(${scale * rowIndex},"${matrix[i].join('')}")`;
     rowIndex++;
   }
 
@@ -80,7 +100,36 @@ export async function transform({
 }: TransformOptions) {
   const image = await Jimp.read(filepath);
   const matrix = await generateMatrix(image, width, height, withoutAlpha);
-  const output = `print("${generateSprites(matrix, scale)}")`;
+  const output = `
+  ¶=function(v,p)
+    l=function(c)
+      if c=="${OptimizedCodes.White}" then
+        return "#FFFFFF"
+      else if c=="${OptimizedCodes.Black}" then
+        return "#000000"
+      else if c=="${OptimizedCodes.Invisible}" then
+        return "#00000000"
+      else if len(c)==4 then
+        return "#"+c[1]+c[1]+c[2]+c[2]+c[3]+c[3]
+      else if len(c)==5 then
+        return "#"+c[1]+c[1]+c[2]+c[2]+c[3]+c[3]+c[4]+c[4]
+      end if
+      return c
+    end function
+    o=""
+    i=indexOf(p,"#")
+    n=null
+    while i!=null
+      n=indexOf(p,"#",i)
+      o=o+"<sprite=0 color="+l(p[i:n])+">"
+      i=n
+    end while
+    if n!=null then o=o+"<sprite=0 color="+l(p[n:])+">"
+    return "<pos=0><voffset="+v+">"+o
+  end function`
+    .split('\n')
+    .map((s) => s.trim())
+    .join(';') + `;print(${generateSprites(matrix, scale)})`;
   const outputPath = path.resolve(outputDirectory, 'image.src');
 
   if (output.length > 160000) {
