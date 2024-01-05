3f803393126561da27c2ea32f8ebc5f334cca611
ayecue <soerenwehmeier@googlemail.com>
2024-01-05 18:12:19 +0100

    0.1.6 minimize output

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@ export enum OptimizedCodes {
   Invisible = '#?'
 }
 
-export function parseMeasurement(arg: string, defaultValue: number = Jimp.AUTO): number {
+export function parseMeasurement(
+  arg: string,
+  defaultValue: number = Jimp.AUTO
+): number {
   if (arg === undefined || arg === '') return defaultValue;
 
   const num = Number(arg);
@@ -21,43 +24,68 @@ export function parseMeasurement(arg: string, defaultValue: number = Jimp.AUTO):
 }
 
 function rbgValueToHexValue(item: number) {
-  var hex = item.toString(16);
+  const hex = item.toString(16);
   return `0${hex}`.slice(-2);
 }
 
 function rgbToHex(r: number, g: number, b: number) {
-  return `#${rbgValueToHexValue(r)}${rbgValueToHexValue(g)}${rbgValueToHexValue(b)}`;
+  return `#${rbgValueToHexValue(r)}${rbgValueToHexValue(g)}${rbgValueToHexValue(
+    b
+  )}`;
 }
 
 function rgbToHexWithAlpha(r: number, g: number, b: number, a: number) {
   return `${rgbToHex(r, g, b)}${rbgValueToHexValue(a)}`;
 }
 
-async function generateMatrix(image: Jimp, width: number = 64, height: number = Jimp.AUTO, withoutAlpha: boolean): Promise<string[][]> {
+function compress(hex: string): string {
+  let output = '#';
+
+  for (let i = 1; i < hex.length; i += 2) {
+    output += String.fromCharCode(100 + parseInt(hex.substring(i, i + 2), 16));
+  }
+
+  return output;
+}
+
+async function generateMatrix(
+  image: Jimp,
+  width: number = 64,
+  height: number = Jimp.AUTO,
+  withoutAlpha: boolean
+): Promise<string[][]> {
   const resizedImage = await image.resize(width, height);
-  const chunks = (new Array(resizedImage.bitmap.height)).fill(undefined).map(() => new Array(resizedImage.bitmap.width));
+  const chunks = new Array(resizedImage.bitmap.height)
+    .fill(undefined)
+    .map(() => new Array(resizedImage.bitmap.width));
 
-  resizedImage.scan(0, 0, resizedImage.bitmap.width, resizedImage.bitmap.height, function (x, y, idx) {
-    const red = this.bitmap.data[idx + 0];
-    const green = this.bitmap.data[idx + 1];
-    const blue = this.bitmap.data[idx + 2];
-    const alpha = this.bitmap.data[idx + 3];
-    const hasNoAlpha = withoutAlpha || alpha === 255;
-    let hex;
+  resizedImage.scan(
+    0,
+    0,
+    resizedImage.bitmap.width,
+    resizedImage.bitmap.height,
+    function (x, y, idx) {
+      const red = this.bitmap.data[idx + 0];
+      const green = this.bitmap.data[idx + 1];
+      const blue = this.bitmap.data[idx + 2];
+      const alpha = this.bitmap.data[idx + 3];
+      const hasNoAlpha = withoutAlpha || alpha === 255;
+      let hex;
 
-    if (hasNoAlpha) {
-      hex = rgbToHex(red, green, blue);
-      if (hex === '#000000') hex = OptimizedCodes.Black;
-      else if (hex === '#FFFFFF') hex = OptimizedCodes.White;
-      else if (hex.charAt(1) === hex.charAt(2) && hex.charAt(3) === hex.charAt(4) && hex.charAt(5) === hex.charAt(6)) hex = `#${hex.charAt(1) + hex.charAt(3) + hex.charAt(5)}`;
-    } else {
-      hex = rgbToHexWithAlpha(red, green, blue, alpha);
-      if (alpha === 0) hex = OptimizedCodes.Invisible;
-      else if (hex.charAt(1) === hex.charAt(2) && hex.charAt(3) === hex.charAt(4) && hex.charAt(5) === hex.charAt(6) && hex.charAt(7) === hex.charAt(8)) hex = `#${hex.charAt(1) + hex.charAt(3) + hex.charAt(5) + hex.charAt(7)}`;
+      if (hasNoAlpha) {
+        hex = rgbToHex(red, green, blue);
+        if (hex === '#000000') hex = OptimizedCodes.Black;
+        else if (hex === '#FFFFFF') hex = OptimizedCodes.White;
+        else hex = compress(hex);
+      } else {
+        hex = rgbToHexWithAlpha(red, green, blue, alpha);
+        if (alpha === 0) hex = OptimizedCodes.Invisible;
+        else hex = compress(hex);
+      }
+
+      chunks[y][x] = hex;
     }
-
-    chunks[y][x] = hex;
-  });
+  );
 
   return chunks;
 }
@@ -93,8 +121,13 @@ export async function transform({
 }: TransformOptions) {
   const image = await Jimp.read(filepath);
   const matrix = await generateMatrix(image, width, height, withoutAlpha);
-  const output = `
+  const output =
+    `
   ¶=function(v,p)
+    b="0123456789abcdef"
+    q=function(z)
+      return b[floor(z/16)]+b[z%16]
+    end function
     l=function(c)
       if c=="${OptimizedCodes.White}" then
         return "#FFFFFF"
@@ -102,12 +135,16 @@ export async function transform({
         return "#000000"
       else if c=="${OptimizedCodes.Invisible}" then
         return "#00000000"
-      else if len(c)==4 then
-        return "#"+c[1]+c[1]+c[2]+c[2]+c[3]+c[3]
-      else if len(c)==5 then
-        return "#"+c[1]+c[1]+c[2]+c[2]+c[3]+c[3]+c[4]+c[4]
       end if
-      return c
+      j=@code
+      r=q(j(c[1])-100)
+      g=q(j(c[2])-100)
+      b=q(j(c[3])-100)
+      if len(c)==5 then
+        a=q(j(c[4])-100)
+        return "#"+r+g+b+a
+      end if
+      return "#"+r+g+b
     end function
     o=""
     i=indexOf(p,"#")
@@ -120,9 +157,9 @@ export async function transform({
     if n!=null then o=o+"<sprite=0 color="+l(p[n:])+">"
     return "<pos=0><voffset="+v+">"+o
   end function`
-    .split('\n')
-    .map((s) => s.trim())
-    .join(';') + `;print(${generateSprites(matrix, scale)})`;
+      .split('\n')
+      .map((s) => s.trim())
+      .join(';') + `;print(${generateSprites(matrix, scale)})`;
   const outputPath = path.resolve(outputDirectory, 'image.src');
 
   if (output.length > 160000) {
